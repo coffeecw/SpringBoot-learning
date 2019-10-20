f76e37fc962029f4fa5f1869c5ce058e8bfe21ce
cw <1689691629@qq.com>
2019-10-20 22:54:06 +0800

    Spring Boot hello world

---
+++ springboot01-helloworld/src/main/java/cn/cwcofffee/HelloWorldApplicationBoot.java
@@ -0,0 +1,15 @@
+package cn.cwcofffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * created by coffeecw 2019/10/14
+ */
+@SpringBootApplication//标注一个主程序类，说明这是一个Spring Boot应用
+public class HelloWorldApplicationBoot {
+    public static void main(String[] args) {
+        //Spring Boot应用启动起来
+        SpringApplication.run(HelloWorldApplicationBoot.class,args);
+    }
+}
---
+++ springboot01-helloworld/src/main/java/cn/cwcofffee/controller/HelloController.java
@@ -0,0 +1,18 @@
+package cn.cwcofffee.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * created by coffeecw 2019/10/14
+ */
+@Controller
+public class HelloController {
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello world";
+    }
+
+}
