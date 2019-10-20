eae020eec7441aa017de801292cca0acc80a490b
cw <1689691629@qq.com>
2019-10-20 23:35:11 +0800

    SpringBoot initializer

---
+++ springboot02-initializer/src/main/java/cn/cwcoffee/springboot02initializer/Springboot02InitializerApplication.java
@@ -0,0 +1,13 @@
+package cn.cwcoffee.springboot02initializer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot02InitializerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot02InitializerApplication.class, args);
+    }
+
+}
---
+++ springboot02-initializer/src/main/java/cn/cwcoffee/springboot02initializer/controller/HelloController.java
@@ -0,0 +1,9 @@
+package java.cn.cwcoffee.springboot02initializer.controller;
+
+/**
+ * created by coffeecw 2019/10/15
+ */
+
+public class HelloController {
+
+}
