fd59114095a6f5655fdaf2679ecae395b73e2b14
cw <1689691629@qq.com>
2019-10-27 15:45:05 +0800

    SpringBoot AutoConfiguration

---
+++ springboot04-autoconfig/src/main/java/cn/cwcoffee/springboot/Springboot04AutoconfigApplication.java
@@ -0,0 +1,13 @@
+package cn.cwcoffee.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot04AutoconfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot04AutoconfigApplication.class, args);
+    }
+
+}
